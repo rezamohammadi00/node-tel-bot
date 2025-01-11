f671e26422b643d28521e7d094522b8cade54ec9
Rez <mohammadireza7979.com>
2025-01-11 04:41:12 +0330

    v0.1

--- dist/server.js
+++ dist/server.js
@@ -1,17 +1,69 @@
-var TelegramBot = require("node-telegram-bot-api");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("path");
+const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
-var TOKEN = process.env.TEL_BOT_TOKEN;
-var bot = new TelegramBot(TOKEN, {
+const contact_1 = __importDefault(require("./data/contact"));
+const TOKEN = process.env.TEL_BOT_TOKEN;
+const bot = new TelegramBot(TOKEN, {
     polling: true,
 });
-bot.onText(/\/start/, function (msg) {
-    console.log(msg.chat.id + " ID" + " user is typing");
-    var chatId = msg.chat.id;
-    bot.sendMessage(chatId, "hiiiiiiiiiiiiiiiii");
+// if user send /start
+bot.onText(/\/start/, async (msg) => {
+    console.log(msg.chat.id + " ID" + " is joined" + " 🤝");
+    const chatId = msg.chat.id;
+    // await   bot.sendPhoto(chatId, path.join(__dirname, "assets", "images", "welcome.jpg"));
+    bot.sendMessage(chatId, "سلام", {
+        reply_markup: {
+            keyboard: [
+                ["عضو هیات علمی گروه علم اطلاعات و دانش شناسی"],
+                ["هیات علمی"],
+                ["رئیس اداره پذیرش"],
+                ["بایگان اداره فارغ التحصیلان"],
+                ["عضو هیات علمی گروه آبیاری و زهکشی"],
+                ["کارشناس گروه حسابداری"],
+                ["کارشناس گروه مدیریت"],
+                ["کارشناس گروه کامپیوتر"],
+                ["کارشناس گروه رشته کامپیوتر"],
+                ["کارشناس خدمات آموزشی"],
+                ["کارگاه جوشکاری"],
+                ["کارشناس آموزش رابط معاونت پژوهشی با دفتر نظارت و ارزیابی دانشگاه"],
+                ["مدیر گروه زبان انگلیسی"],
+                ["هیات علمی"],
+                ["رئیس اداره پذیرش"],
+                ["بایگان اداره فارغ التحصیلان"],
+                ["عضو هیات علمی گروه آبیاری و زهکشی"],
+                ["کارشناس گروه حسابداری"],
+                ["کارشناس گروه مدیریت"],
+                ["کارشناس گروه کامپیوتر"],
+                ["کارشناس گروه رشته کامپیوتر"],
+                ["کارشناس خدمات آموزشی"],
+                ["کارگاه جوشکاری"],
+                ["کارشناس آموزش رابط معاونت پژوهشی با دفتر نظارت و ارزیابی دانشگاه"],
+                ["مدیر گروه زبان انگلیسی"],
+            ],
+        },
+    });
+    bot.sendMessage(chatId, "نام بخش مورد نظر را برای دریافت شماره تلفن وارد کنید");
 });
-bot.on("message", function (msg) {
+// if user send any message
+bot.on("message", (msg) => {
     console.log(msg);
-    var chatId = msg.chat.id;
-    bot.sendPhoto(chatId, "AgACAgQAAxkBAANSZ4GVTofy6Ap00mPlhn3bAVHJQdgAAvPFMRvSLRFQ_wWG7T2tosgBAAMCAAN5AAM2BA");
+    const chatId = msg.chat.id;
+    const text = msg.text;
+    const tell = contact_1.default.find((item) => item["واحد سازمانی"] === text);
+    console.log(tell);
+    if (tell && text !== "/start") {
+        bot.sendMessage(chatId, tell["شماره داخلی"]);
+    }
+    else {
+        if (text !== "/start") {
+            bot.sendMessage(chatId, "نام بخش مورد نظر یافت نشد");
+            bot.sendMessage(chatId, "از دکمه های موجود استفاده کنید");
+        }
+    }
 });
 console.log("bot is running");
